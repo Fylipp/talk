Fylipp/talk

commit 15ee365816b5a5f82a302ae503182a284102a6a8

Extract name generator adjectives and nouns

chat.js
@@ -2,16 +2,12 @@
 
 const WebSocket = require('ws')
 
-function generateName() {
-    const adjs = ['Curious', 'Red', 'Tall', 'Shallow', 'Redundant', 'Wild', 'Old', 'Fresh', 'Friendly', 'Wet', 'Crazy', 'Delicious', 'Hairy', 'Fast']
-    const nouns = ['Kitten', 'House', 'Dog', 'Puppy', 'Snow', 'Smith', 'Lion', 'Fridge', 'Tornado', 'Giant', 'Snowman', 'Toad', 'Eagle', 'Hedgehog']
-
-    const random = array => array[Math.floor(Math.random() * array.length)]
-
-    return `${random(adjs)} ${random(nouns)}`
-}
-
 exports.init = (config, server) => {
+    function generateName() {
+        const random = array => array[Math.floor(Math.random() * array.length)]
+        return `${random(config.nameGeneratorAdjectives)} ${random(config.nameGeneratorAdjectives)}`
+    }
+
     const wss = new WebSocket.Server({ server: server })
 
     wss.on('listening', () => {

config.js
@@ -31,5 +31,11 @@ module.exports = {
         'client.js': {
             mime: 'application/javascript'
         }
-    }
+    },
+
+    // The adjectives availible to the name generator
+    nameGeneratorAdjectives: ['Curious', 'Red', 'Tall', 'Shallow', 'Redundant', 'Wild', 'Old', 'Fresh', 'Friendly', 'Wet', 'Crazy', 'Delicious', 'Hairy', 'Fast'],
+
+    // The nouns availible to the name generator
+    nameGeneratorNouns: ['Kitten', 'House', 'Dog', 'Puppy', 'Snow', 'Smith', 'Lion', 'Fridge', 'Tornado', 'Giant', 'Snowman', 'Toad', 'Eagle', 'Hedgehog']
 }

commit f696037b637447b7bfbe18351f0130b0f3afb5af

Fix name generation error
The name generator change in 15ee365816b5a5f82a302ae503182a284102a6a8
broke the generator, as it combined two adjectives instead of
an adjective and a noun.

chat.js
@@ -5,7 +5,7 @@ const WebSocket = require('ws')
 exports.init = (config, server) => {
     function generateName() {
         const random = array => array[Math.floor(Math.random() * array.length)]
-        return `${random(config.nameGeneratorAdjectives)} ${random(config.nameGeneratorAdjectives)}`
+        return `${random(config.nameGeneratorAdjectives)} ${random(config.nameGeneratorNouns)}`
     }
 
     const wss = new WebSocket.Server({ server: server })